6e787f49995147806b22b758bd7f0ad0332b3cd4
Anny Shan <annyshan@annys-mbp.fios-router.home>
2019-01-14 10:17:39 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Anny Shan on 1/14/19.
+//  Copyright © 2019 Jun Shan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
