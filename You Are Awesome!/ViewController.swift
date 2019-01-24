7338bfdee5c61b76ce9c0461d7f0b8f526e98c4b
Anny Shan <annyshan@annys-mbp.bc.edu>
2019-01-24 15:07:22 -0500

    2.3 Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    //Code below executes when the app's view first load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //
+        print("The View Loaded!")
     }
 
-
+    @IBAction func showMessageButtonPressed(_ sender: UIButton) {
+        print("*** The button was clicked")
+        messageLabel.text = "You Are Awesome!"
+    }
+    
+    
 }
 
